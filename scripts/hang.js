8607aab65378c07f2b1b317cce68e13a30500d76
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-25 22:51:51 +0300

    created addHang part function

---
+++ scripts/hang.js
@@ -0,0 +1,26 @@
+const hangSection = document.getElementById("hang");
+
+function addHang(attempts) {
+  var part = document.createElement("img");
+  if (attempts === 1) {
+    part.classList.add("head");
+    part.src="./assets/head.svg"
+  } else if (attempts === 2) {
+    part.classList.add("body");
+    part.src="./assets/body.svg"
+  } else if (attempts === 3) {
+    part.classList.add("left-hand");
+    part.src="./assets/left-hand.svg"
+  } else if (attempts === 4) {
+    part.classList.add("right-hand");
+    part.src="./assets/right-hand.svg"
+  } else if (attempts === 5) {
+    part.classList.add("left-leg");
+    part.src="./assets/left-leg.svg"
+
+  } else if (attempts === 6) {
+    part.classList.add("right-leg");
+    part.src="./assets/right-leg.svg"
+  }
+  hangSection.appendChild(part)
+}
